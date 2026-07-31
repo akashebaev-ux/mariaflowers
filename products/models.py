efc6b170d18bbc055f4175064bd5f32e69757a3c
akashebaev-ux <akashebaev@gmail.com>
2026-07-31 23:10:57 +0200

    Add configurable extra flower pricing

--- products/models.py
+++ products/models.py
@@ -1,13 +1,18 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
@@ -17,15 +22,73 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)  # noqa
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey(
+        "Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    sku = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True,
+    )
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True) # noqa
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+
+    has_sizes = models.BooleanField(
+        default=False,
+        null=True,
+        blank=True,
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+        blank=True,
+    )
+
+    image = models.ImageField(
+        null=True,
+        blank=True,
+    )
+
+    included_flower_count = models.PositiveIntegerField(
+        default=25,
+        help_text="Number of flowers included in the bouquet",
+    )
+
+    extra_flower_price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+        validators=[MinValueValidator(0)],
+        help_text="Price for each additional flower",
+    )
+
+    max_extra_flowers = models.PositiveIntegerField(
+        default=300,
+        help_text="Maximum number of additional flowers",
+    )
+
+    @property
+    def maximum_flower_count(self):
+        return (
+            self.included_flower_count
+            + self.max_extra_flowers
+        )
 
     def __str__(self):
         return self.name
