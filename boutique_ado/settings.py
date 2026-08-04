03651dc271e82fdfe5301a3171357ff179b84ef9
akashebaev-ux <akashebaev@gmail.com>
2026-08-04 22:29:40 +0200

    Add WhatsApp environment settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -235,6 +235,15 @@ else:
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
+# WhatsApp
+WHATSAPP_API_URL = os.getenv("WHATSAPP_API_URL", "")
+WHATSAPP_ACCESS_TOKEN = os.getenv("WHATSAPP_ACCESS_TOKEN", "")
+WHATSAPP_SENDER_ID = os.getenv("WHATSAPP_SENDER_ID", "")
+WHATSAPP_ORDER_RECIPIENT = os.getenv(
+    "WHATSAPP_ORDER_RECIPIENT",
+    "",
+)
+
 # --------------------------------------------------------
 # Security settings
 # --------------------------------------------------------
