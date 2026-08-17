49bf52a49148c57c33097b3374f1d723e238f3d0
Azamat Kashebayev <AKashebaev@gmail.com>
2026-08-17 22:51:38 +0300

    Add contact form view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -6,3 +9,29 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """Display and process the customer contact form."""
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(
+                request,
+                "Thank you. Your message has been received."
+            )
+
+            return redirect("contact")
+
+    else:
+        form = ContactForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "home/contact.html", context)
