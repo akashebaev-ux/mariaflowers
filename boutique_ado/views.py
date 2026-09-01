7b9fb791257198e91c72494c110cac803e9ddc8d
Azamat Kashebayev <AKashebaev@gmail.com>
2026-09-02 02:00:27 +0500

    Add custom 500 error page

--- boutique_ado/views.py
+++ boutique_ado/views.py
@@ -20,6 +20,11 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
+def handler500(request):
+    """Error Handler 500 - Internal Server Error."""
+    return render(request, "errors/500.html", status=500)
+
+
 def privacy_policy(request):
     """Display the Maria Flowers privacy policy."""
     return render(request, "privacy_policy.html")
